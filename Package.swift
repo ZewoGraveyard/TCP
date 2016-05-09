3aa626f0d9abf8e13001355f7fa93abda54a7e7b
Tomohisa Takaoka <tomohisa.takaoka@gmail.com>
2016-05-10 00:40:54 +0900

    Update for 2016-05-03 Snapshot (#7) (#8)
    * testing for https crash
    
    workaround for
    
    https://github.com/Zewo/Zewo/issues/81#issuecomment-215141561
    
    * HTTPS closed on some client
    
    some client reset tcp connection after finish sending ssl response.
    
    To avoid exception, when reset connection, close stream and it go
    through fine.
    
    * update for 05-03 snapshot
    
    only changes dependency
    
    * change .swift-version
    
    * Update for 2016-05-03

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "TCP",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/IP.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/VeniceX/IP.git", majorVersion: 0, minor: 7),
     ]
 )
