55e69f60d45a2ab37441e63b207a4691769a43f0
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-07-20 00:17:17 +0200

    Updated Swift and dependency

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "TCP",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/IP.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/VeniceX/IP.git", majorVersion: 0, minor: 9),
     ]
 )
